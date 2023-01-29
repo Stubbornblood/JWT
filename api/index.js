ac27ec3470afe1aa47571e16541ed5a03397e4aa
Stubbornblood <ss3591419@gmail.com>
2023-01-29 17:02:17 +0530

    Created Token with jwt check wheather it's working or not

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
 app.use(express.json())
 
 
@@ -29,9 +30,54 @@ app.post("/api/login",(req,res)=>{
     });
 
     if(user){
-        res.json(user)
+         
+        //Generte and access token
+        const accessToken = jwt.sign({id:user.id,isAdmin:user.isAdmin},"mySecretKey")
+        
+        res.json({
+            username:username,
+            isAdmin:user.isAdmin,
+            accessToken,
+        })
+ 
+         
+
     }else{
         res.status(400).json("Username or password incorrect!")
+    }
+})
+
+
+
+const verify = (req,res,next) =>{
+     const authHeader = req.headers.authorization;
+     if(authHeader){
+       
+        const token = authHeader.split(" ")[1];
+
+
+        jwt.verify(token,"mySecretKey",(err,user)=>{
+            if(err){
+                return res.status(403).json("Token is invalid")
+            }
+
+            req.user=user;
+            next();
+        })
+         
+
+
+     }else{
+        res.status(403).json("You are not authenticate!");
+     }
+}
+
+
+app.delete("/api/users/:userid",verify,(req,res)=>{
+    if(req.user.id===req.params.userid||req.user.isAdmin){
+        res.status(200).json("user has been deleted.")
+    }else{
+        res.status(403).json("You are not allowed to delete this user")
     }
 })
 
